5709fbcc5726fec182f20b4f5bdc202d01766e24
Paulo Victor <pvcunha05@gmail.com>
2021-04-19 14:31:17 -0300

    Set up & Dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.json({
+    message: 'Hello World !',
+  });
+});
+
+app.post('/users', (request, response) => {
+  const { name } = request.body;
+
+  const user = { name };
+
+  response.json(user);
+});
 
 const PORT = process.env.PORT || 3333;
 
